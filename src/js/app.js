508b9243feffd6e97e959eaf0a7501605c9bafb1
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2022-02-14 20:08:46 +0100

    Initial CV

---
+++ src/js/app.js
@@ -0,0 +1,57 @@
+//HTML Elements
+const navList = document.querySelector(".side-bar ul");
+const myImg = document.querySelector(".my-img-sidebar");
+
+const sectionsNames = [
+  "information",
+  "min-profil",
+  "kompentens",
+  "projekt",
+  "utbildningar",
+  "språk",
+];
+
+//fixed navList
+const navListInterSection = (entries) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting && screen.width > 992)
+    navList.classList.add("fixed-ul");
+  else navList.classList.remove("fixed-ul");
+};
+
+//Observer
+const navListObserver = new IntersectionObserver(navListInterSection, {
+  root: null,
+  threshold: 0,
+});
+
+navListObserver.observe(myImg);
+
+sectionsNames.forEach((secName) => {
+  const section = document.getElementById(`${secName}-sec`);
+  const sectionHeader = section.querySelector(".h-section");
+
+  const interSection = (entries) => {
+    const [entry] = entries;
+
+    if (entry.isIntersecting) section.classList.remove("invisible-sec");
+  };
+
+  const secNameObserver = new IntersectionObserver(interSection, {
+    root: null,
+    threshold: 1,
+  });
+
+  secNameObserver.observe(sectionHeader);
+});
+
+//Events
+sectionsNames.forEach((secName) => {
+  const btn = document.getElementById(`${secName}-btn`);
+  const section = document.getElementById(`${secName}-sec`);
+
+  btn.addEventListener("click", () =>
+    section.scrollIntoView({ behavior: "smooth" })
+  );
+});
